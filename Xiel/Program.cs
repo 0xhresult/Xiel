c9ccb4978c61848d789ffa09b9222463475fd9bb
Adam <0xhresult@gmail.com>
2014-04-07 23:48:22 +0200

    1

--- Xiel/Program.cs
+++ Xiel/Program.cs
@@ -1,7 +1,24 @@
 ﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+
+[assembly: AssemblyTitle("Xiel")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Xiel")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("c538bb94-c8c6-4751-b146-3c8daf4b9f67")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
 
 namespace Xiel
 {
+
     static class Program
     {
         [STAThread]
